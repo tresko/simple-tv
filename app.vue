0fea6a9f1b438fa3042eef95517be4296fe6eeb1
Miha Sedej <sedej.miha@gmail.com>
2023-03-10 20:03:13 +0100

    Test SWR

--- app.vue
+++ app.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    test
+    Date from api
     <template v-if="data">
       {{ data.api }}
     </template>
+
+    <br />
+    Date from client {{ new Date().getTime() }}
   </div>
 </template>
 

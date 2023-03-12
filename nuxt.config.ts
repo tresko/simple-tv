7073c23cf0adbbfb70deffcb3eacff47b54d9f2e
Miha Sedej <sedej.miha@gmail.com>
2023-03-12 16:32:53 +0100

    Add swr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,12 +8,12 @@ export default defineNuxtConfig({
 
   nitro: {
     routeRules: {
-      // "/api/ohlc/**": {
-      //   swr: 60,
-      // },
-      // "/api/symbols": {
-      //   swr: 60 * 60,
-      // },
+      "/api/ohlc/**": {
+        swr: 60,
+      },
+      "/api/symbols": {
+        swr: 60 * 60,
+      },
     },
   },
 });
